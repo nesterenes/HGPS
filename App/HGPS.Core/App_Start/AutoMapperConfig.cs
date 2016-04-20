2d7bb43496e264cdf78c8586b16837d07e013be9
Neil Shaffer <neoshaffer@gmail.com>
2016-04-19 21:59:30 -0400

    adding automapper

---
+++ App/HGPS.Core/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using HGPS.Data.ModelsEF;
+
+namespace HGPS.Core
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            // --------------------------------------------------------
+            // TO MODEL OBJECTS
+            // --------------------------------------------------------
+
+            //AutoMapper.Mapper.CreateMap<ActivityLog, ActivityLogModel>()
+            //    .ForMember(dest => dest.DateOccurred, opts => opts.MapFrom(src => src.DateOccurred.ToString("MM/dd/yyyy")));
+
+            //AutoMapper.Mapper
+
+
+            // --------------------------------------------------------
+            // TO DATABASE OBJECTS
+            // --------------------------------------------------------
+
+
+
+        }
+
+    }
+}
